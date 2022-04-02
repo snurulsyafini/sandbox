ab7fa7650ab7b723aa4265aa38b30fd52a3d8fdb
Nurul Syafini <nurulsyafinisubhan.bintemohamedaidilsubhan@my.jcu.edu.au>
2022-04-02 17:03:18 +0800

    Add docstrings to password checker

--- password_check.py
+++ password_check.py
@@ -1,8 +1,17 @@
+"""
+Description: A simple password checker
+
+Name: Nurul Syafini Subhan
+"""
+
+
 def is_valid_password(text):
+    """Check whether a given text has the correct password format"""
     return 8 <= len(text) <= 20
 
 
 def main():
+    """Start program"""
     new_password = input("Input password: ")
     new_password = len(new_password)
     for i in range(new_password):
